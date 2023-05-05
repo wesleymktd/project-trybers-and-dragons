252a85a8b545d9c40451fa46b21d29f4d4eacb27
wesleymktd <wesleymktd@gmail.com>
2023-05-05 17:27:38 -0300

    created class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static notImplement() {
+    throw new Error('Not implemented');
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
