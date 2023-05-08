f9828879d0dd34306bc5b4e50c07206f8e6ae9f6
wesleymktd <wesleymktd@gmail.com>
2023-05-08 16:19:08 -0300

    req 09 ok class Monster

---
+++ src/Monster.ts
@@ -0,0 +1,34 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this._lifePoints - attackPoints;
+    if (damage <= 0) {
+      this._lifePoints = -1;  
+    } else {
+      this._lifePoints = damage;
+    }
+
+    return this._lifePoints;
+  }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
